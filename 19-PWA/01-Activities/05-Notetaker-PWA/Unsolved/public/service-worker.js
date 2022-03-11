e2e97892f2f302d550fc45f457bf6d6e30735978
amarten <alexdmarten@gmail.com>
2022-03-10 21:03:57 -0800

    thursday activities

--- 19-PWA/01-Activities/05-Notetaker-PWA/Unsolved/public/service-worker.js
+++ 19-PWA/01-Activities/05-Notetaker-PWA/Unsolved/public/service-worker.js
@@ -1,29 +1,20 @@
-const CACHE_NAME = 'my-site-cache-v1';
-const DATA_CACHE_NAME = 'data-cache-v1';
+const CACHE_NAME = "my-site-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v2";
 
 const FILES_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/favicon.ico',
-  '/manifest.json',
-  '/assets/css/style.css',
-  '/assets/js/loadImages.js',
-  '/assets/images/icons/icon-72x72.png',
-  '/assets/images/icons/icon-96x96.png',
-  '/assets/images/icons/icon-128x128.png',
-  '/assets/images/icons/icon-144x144.png',
-  '/assets/images/1.jpg',
-  '/assets/images/2.jpg',
-  '/assets/images/3.jpg',
-  '/assets/images/4.jpg',
-  '/assets/images/5.jpg',
-  '/assets/images/6.jpg',
-  '/assets/images/7.jpg',
-  '/assets/images/8.jpg',
-  '/assets/images/9.jpg'
+    '/',
+    '/index.html',
+    '/favicon.ico',
+    '/manifest.json',
+    '/app.js',
+    '/assets/css/style.css',
+    '/assets/images/icons/icon-72x72.png',
+    '/assets/images/icons/icon-96x96.png',
+    '/assets/images/icons/icon-128x128.png',
+    '/assets/images/icons/icon-144x144.png'
 ];
 
-// Install the service worker
+// Install service worker
 self.addEventListener('install', function(e) {
     e.waitUntil(
         caches.open(CACHE_NAME).then(cache => {
@@ -35,7 +26,7 @@ self.addEventListener('install', function(e) {
     self.skipWaiting();
 })
 
-// Activate the service worker and remove old data from the cache
+// Activate service worker
 self.addEventListener('activate', function(e) {
     e.waitUntil(
         caches.keys().then(keyList => {
